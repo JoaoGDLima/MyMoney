3de648817bf6781e69bb6f624a474e233785d967
JoaoGDLima <joao.lima@universo.univates.br>
2021-06-23 21:25:50 -0300

    Utilização do postgres

--- MyMoney/myMoney/settings.py
+++ MyMoney/myMoney/settings.py
@@ -66,9 +66,17 @@ WSGI_APPLICATION = 'myMoney.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
