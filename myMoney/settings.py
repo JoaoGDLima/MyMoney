923d993d4370fc448da3daf58dfd0083335e8207
JoaoGDLima <joao.lima@universo.univates.br>
2021-06-30 12:52:45 -0300

    Teste

--- myMoney/settings.py
+++ myMoney/settings.py
@@ -80,19 +80,13 @@ DATABASES = {
     #    'PORT': 5432,
     #}
 
-    if 'DB_HOST' in os.environ:
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'HOST': os.environ.get('DB_HOST'),
-            'NAME': os.environ.get('DB_NAME'),
-            'USER': os.environ.get('DB_USER'),
-            'PASSWORD': os.environ.get('DB_PASS'),
-        }
-    else:
-       'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }     
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASS'),
+    }
 }
 
 
